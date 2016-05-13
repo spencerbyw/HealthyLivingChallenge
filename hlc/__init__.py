59192a9b80388979ab598238c69ffded69adec15
Spencer Bywater <spencer.bywater@gmail.com>
2016-05-12 20:23:17 -0700

    Allowing better API access.

--- hlc/__init__.py
+++ hlc/__init__.py
@@ -1,19 +1,22 @@
-from flask import Flask, jsonify
+import settings
+
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "In development: Healthy Living Challenge"
+	return "In development: Healthy Living Challenge"
 
 
 @app.route("/api/test", methods=['GET'])
 def test_json():
-    # response = make_response({'stuff': [1, 2, 3, 'hedgehog']})
-    # response.headers['Access-Control-Allow-Origin'] = "*"
-    return jsonify({"yes": "no"})
-    # return response
+	response = make_response(jsonify({'stuff': [1, 2, 3, 'hedgehog']}))
+	response.headers['Access-Control-Allow-Origin'] = "*"
+	# return jsonify({"yes": "no"})
+	return response
 
 if __name__ == "__main__":
-    app.run()
+	app.debug = settings.ENABLE_DEBUG
+	app.run()
 
